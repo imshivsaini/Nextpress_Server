86268375fdb49da39648b40fa87cbdca33fa96e7
abitmoreofaksh <akshkkr1@gmail.com>
2025-06-24 12:04:17 +0530

    Get specific and Update specific API + cors setup

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import "./connection.js";
 import url from "./Models/url.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.post("/add-url", async (req, res) => {
@@ -23,8 +25,37 @@ app.post("/add-url", async (req, res) => {
 
 app.get("/get-url", async (req, res) => {
   try {
-    const url = await url.findAll();
-    return res.status(200).json({ success: true, data: url });
+    const data = await url.find();
+    return res.status(200).json({ success: true, data: data });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ success: false, error: err });
+  }
+});
+
+app.get("/get-specific/:url", async (req, res) => {
+  try {
+    const data = await url.findOne({ url: req.params.url });
+    return res.status(200).json({ success: true, data: data });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ success: false, error: err });
+  }
+});
+
+app.put("/update-specific/:url", async (req, res) => {
+  try {
+    const { content, root } = req.body;
+    const data = await url.findOne({ url: req.params.url });
+    if (!data) {
+      return res.status(404).json({ success: false, message: "Not found" });
+    }
+    data.content = content;
+    data.root = root;
+    await data.save();
+    return res
+      .status(200)
+      .json({ success: true, message: "Updated Successfully" });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ success: false, error: err });
