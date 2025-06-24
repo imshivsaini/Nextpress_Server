151686466b8bf386473e48318ee355c8bc55dbf9
abitmoreofaksh <akshkkr1@gmail.com>
2025-06-24 17:29:44 +0530

    solved bugs

--- controller/nextpress.js
+++ controller/nextpress.js
@@ -1,42 +1,50 @@
 import url from "../Models/url.js";
-export const AddUrl = async(req,res) =>{
-    try {
+
+export const AddUrl = async (req, res) => {
+  try {
     const response = await url.findOne({
       url: req.body.url,
     });
     if (response) {
-      return res.status(401).json({ success: false, message: "Already Exists" });
+      return res
+        .status(401)
+        .json({ success: false, message: "Already Exists" });
     }
     await url.create(req.body);
-    return res.status(200).json({ success: true, message: "Added Successfully" });
+    return res
+      .status(200)
+      .json({ success: true, message: "Added Successfully" });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ success: false, message: "Server Error" });
   }
-}
+};
 
-export const GetUrl = async(req,res) =>{
-try {
+export const GetUrl = async (req, res) => {
+  try {
     const data = await url.find();
     return res.status(200).json({ success: true, data: data });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ success: false, error: err });
   }
-}
+};
 
-export const GetSpeUrl = async(req,res) =>{
-    try {
+export const GetSpeUrl = async (req, res) => {
+  try {
     const data = await url.findOne({ url: req.params.url });
+    if (!data) {
+      return res.status(404).json({ success: false, message: "No data found" });
+    }
     return res.status(200).json({ success: true, data: data });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ success: false, error: err });
   }
-}
+};
 
-export const UpdateSpeUrl = async(req,res) => {
-    try {
+export const UpdateSpeUrl = async (req, res) => {
+  try {
     const { content, root } = req.body;
     const data = await url.findOne({ url: req.params.url });
     if (!data) {
@@ -52,4 +60,4 @@ export const UpdateSpeUrl = async(req,res) => {
     console.log(err);
     return res.status(500).json({ success: false, error: err });
   }
-}
+};
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import "./connection.js";
-import url from "./Models/url.js";
 import cors from "cors";
 import NextPress from "./routes/nextpress.js";
 
@@ -8,8 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api",NextPress);
-
+app.use("/api", NextPress);
 
 app.listen(8000, () => {
   console.log("Server Chal rha h");
